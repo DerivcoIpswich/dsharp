6db5e74e1054b2f29d1f6589062936bc33cdd868
rosariocr <Rosario_Crisci@derivco.co.uk>
2019-07-05 12:20:11 +0200

    Comments on List.cs

--- src/DSharp.Mscorlib/Collections/Generic/List.cs
+++ src/DSharp.Mscorlib/Collections/Generic/List.cs
@@ -10,7 +10,8 @@ namespace System.Collections.Generic
     [ScriptIgnoreNamespace]
     [ScriptImport]
     [ScriptName("Array")]
-    public sealed class List<T> : ICollection<T>
+    // CLR Reference https://source.dot.net/#System.Private.CoreLib/shared/System/Collections/Generic/List.cs,cf7f4095e4de7646,references
+    public sealed class List<T> : ICollection<T> /*IList<T>, IList, IReadOnlyList<T>*/ //NOTE: IList<T> inherits from ICollection<T>
     {
         public List()
         {
@@ -20,7 +21,7 @@ namespace System.Collections.Generic
         {
         }
 
-        public List(params T[] items)
+        public List(params T[] items) /*items type should be IEnumerable<T> and not params T*/
         {
         }
 
@@ -35,30 +36,46 @@ namespace System.Collections.Generic
         public extern void Add(T item);
 
         [ScriptName("push")]
-        public extern void AddRange(params T[] items);
+        public extern void AddRange(params T[] items); /*items type should be IEnumerable<T> and not params T*/
 
         public extern void Clear();
 
+        //This method does not exist in CLR List
         public extern List<T> Concat(params T[] objects);
 
         public extern bool Contains(T item);
-
+        
+        //This method does not exist in CLR List
         public extern bool Every(ListFilterCallback<T> filterCallback);
-
+        
+        //This method does not exist in CLR List
         public extern bool Every(ListItemFilterCallback<T> itemFilterCallback);
 
+        //This method does not exist in CLR List
         public extern List<T> Filter(ListFilterCallback<T> filterCallback);
 
+        //This method does not exist in CLR List
         public extern List<T> Filter(ListItemFilterCallback<T> itemFilterCallback);
 
+        //In CLR we just have a single ForEach method that takes Action<T> as parameter
         public extern void ForEach(ListCallback<T> callback);
 
         public extern void ForEach(ListItemCallback<T> itemCallback);
 
+        //GetEnumerator in CLR is implemented like this: 
+        //public Enumerator GetEnumerator()
+        //    => new Enumerator(this);
+
+        //IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        //    => new Enumerator(this);
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //    => new Enumerator(this);
         public extern IEnumerator<T> GetEnumerator();
 
         extern IEnumerator IEnumerable.GetEnumerator();
 
+        //This method doesn't exist in the CLR
         public extern List<T> GetRange(int index);
 
         public extern List<T> GetRange(int index, int count);
@@ -67,24 +84,36 @@ namespace System.Collections.Generic
 
         public extern int IndexOf(T item, int startIndex);
 
+        //In CLR we also have this signature -> IndexOf(T item, int index, int count)
+
         public extern void Insert(int index, T item);
 
+        //Instead of params T[] it should be -> IEnumerable<T> collection
         public extern void InsertRange(int index, params T[] items);
 
+        //This doesn't exist in CLR
         public extern string Join();
 
+        //This doesn't exist in CLR
         public extern string Join(string delimiter);
 
+        // arg should be -> T item
         public extern int LastIndexOf(object item);
-
+        
+        // arg should be -> T item
         public extern int LastIndexOf(object item, int fromIndex);
 
+        // In CLR we have an additional LastIndexOf method -> public int LastIndexOf(T item, int index, int count)
+
+        // There's no Map method in CLR
         public extern List<TTarget> Map<TTarget>(ListMapCallback<T, TTarget> mapCallback);
 
         public extern List<TTarget> Map<TTarget>(ListItemMapCallback<T, TTarget> mapItemCallback);
 
+        // There's not Parse in CLR
         public extern static List<T> Parse(string s);
 
+        // There's no Reduce methods in CLR
         public extern TReduced Reduce<TReduced>(ListReduceCallback<TReduced, T> callback);
 
         public extern TReduced Reduce<TReduced>(ListReduceCallback<TReduced, T> callback, TReduced initialValue);
@@ -106,26 +135,37 @@ namespace System.Collections.Generic
 
         public extern void RemoveAt(int index);
 
+        // RemoveRange, in CLR, returns void
         public extern List<T> RemoveRange(int index, int count);
 
         public extern void Reverse();
 
+        // There's an additional Reverse method in CLR -> public void Reverse(int index, int count)
+
+        // There's no Slice methods in CLR
         public extern List<T> Slice(int start);
 
         public extern List<T> Slice(int start, int end);
 
+        // There's no Some methods in CLR
         public extern bool Some(ListFilterCallback<T> filterCallback);
 
         public extern bool Some(ListItemFilterCallback<T> itemFilterCallback);
 
         public extern void Sort();
 
+        // The compare callback should be instead -> IComparer<T>? comparer
         public extern void Sort(CompareCallback<T> compareCallback);
+        
+        // There are additional Sort methods in CLR
+
+        // There's no Splice methods in CLR
 
         public extern void Splice(int start, int deleteCount);
 
         public extern void Splice(int start, int deleteCount, params T[] itemsToInsert);
 
+        // There's no Unshift methods in CLR
         public extern void Unshift(params T[] items);
 
         [ScriptSkip]
@@ -140,5 +180,8 @@ namespace System.Collections.Generic
         public extern static explicit operator ArrayList(List<T> list);
 
         public extern static explicit operator List<T>(T[] array);
+
+        // NOTE: considering the amount of not-CLR methods we have in this class, is it worth to have a different class, 
+        // inheriting from this, to support and isolate all of the not-CLR functionalities ?
     }
 }
