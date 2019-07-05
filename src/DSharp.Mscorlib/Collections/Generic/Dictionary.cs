f062b37746e6685af96f8983623f7c076cf5e824
rosariocr <Rosario_Crisci@derivco.co.uk>
2019-07-05 14:16:02 +0200

    comments added on Dictionary<K,V>

--- src/DSharp.Mscorlib/Collections/Generic/Dictionary.cs
+++ src/DSharp.Mscorlib/Collections/Generic/Dictionary.cs
@@ -8,7 +8,10 @@ namespace System.Collections.Generic
     [ScriptIgnoreNamespace]
     [ScriptImport]
     [ScriptName("Object")]
-    public sealed class Dictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    // CLR version https://source.dot.net/#System.Private.CoreLib/shared/System/Collections/Generic/IDictionary.cs,20343df0c96b629b,references
+    // In CLR this is an interface (IDictionary) therefore no ctors are defined.
+    public sealed class Dictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>> // In CLR is inheriting from ICollection<KeyValuePair<TKey, TValue>> where TKey : notnull.
+                                                                                           // ICollection<KeyValuePair<TKey, TValue>> inherits from IEnumerable<T>
     {
         public Dictionary() { }
 
@@ -17,9 +20,11 @@ namespace System.Collections.Generic
         [Obsolete("This is only for use by the c# compiler, and cannot be used for generating script.", /* error */ true)]
         public extern Dictionary(int count);
 
-        public extern int Count { get; }
+        public extern int Count { get; }  // There's no Count property on the CLR
 
-        public extern IReadonlyCollection<TKey> Keys { get; }
+        public extern IReadonlyCollection<TKey> Keys { get; }  // Return type should be ICollection<TKey>
+
+        // We are missing the Values property -> ICollection<TValue> Values { get; }
 
         [ScriptField]
         public extern TValue this[TKey key] { get; set; }
@@ -33,17 +38,21 @@ namespace System.Collections.Generic
         [DSharpScriptMemberName("keyExists")]
         public extern bool ContainsKey(TKey key);
 
-        public extern static Dictionary<TKey, TValue> GetDictionary(object o);
+        public extern static Dictionary<TKey, TValue> GetDictionary(object o);  // There's no GetDictionary in the clr. As per Dictionary: if this is a method to convert a 
+                                                                                // js object to a Dictionary, shouldn't GetDictionary belong to a different class?
 
-        public extern IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();
+        public extern IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();  // This doesn't exist in CLR
 
-        extern IEnumerator IEnumerable.GetEnumerator();
+        extern IEnumerator IEnumerable.GetEnumerator(); // This doesn't exist in CLR
 
-        public extern void Remove(TKey key);
+        public extern void Remove(TKey key);  // This should return  bool
 
         [Obsolete("This is only for use by the c# compiler, and cannot be used for generating script.", /* error */ true)]
-        public extern bool TryGetValue(TKey key, out TValue value);
+        public extern bool TryGetValue(TKey key, out TValue value);  // In CLR, the out value is decorated with [MaybeNullWhen(false)] attribute. This is used for
+                                                                     // nullbale reference types, introduced in C# 8. We don't need to support it. 
+                                                                     // TryGetValue should be ok with the current signature.
 
+        // Are we sure we want to be able to cast from Dictionary to Dictionary<T,V> and viceversa? This sounds like js.
         public extern static implicit operator Dictionary(Dictionary<TKey, TValue> dictionary);
 
         public extern static implicit operator Dictionary<TKey, TValue>(Dictionary dictionary);
