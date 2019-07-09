302bf7c298b0a443f8cd26d41f8f3ed36195f7a8
rosariocr <Rosario_Crisci@derivco.co.uk>
2019-07-09 17:01:46 +0200

    all of the Obsolete attributes that I put on Array.cs are now configured not to error.

--- src/DSharp.Mscorlib/Array.cs
+++ src/DSharp.Mscorlib/Array.cs
@@ -17,28 +17,28 @@ namespace System
 
         [ScriptField]
         [ScriptName("length")]
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard",  error: false)]
         public extern int Count { get; }
 
         [ScriptField]
         public extern object this[int index] { get; set; }
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Concat(params object[] objects);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern bool Contains(object item);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern bool Every(ArrayFilterCallback filterCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern bool Every(ArrayItemFilterCallback itemFilterCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Filter(ArrayFilterCallback filterCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Filter(ArrayItemFilterCallback itemFilterCallback);
 
         // I guess these two for each methods are just js forEach.
@@ -49,10 +49,10 @@ namespace System
 
         public extern IEnumerator GetEnumerator();
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array GetRange(int index);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array GetRange(int index, int count);
 
         // IndexOf is always static in CLR
@@ -62,10 +62,10 @@ namespace System
         // The most similar in CLR is -> static int IndexOf(Array array, object? value, int startIndex)
         public extern int IndexOf(object item, int startIndex);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern string Join();
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern string Join(string delimiter);
 
         // LastIndexOf is always static in CLR
@@ -75,56 +75,56 @@ namespace System
         // The most similar in CLR is -> static int LastIndexOf(Array array, object? value, int startIndex)
         public extern int LastIndexOf(object item, int fromIndex);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Map(ArrayMapCallback mapCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Map(ArrayItemMapCallback mapItemCallback);
 
         [DSharpScriptMemberName("array")]
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern static Array Parse(string s);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object Reduce(ArrayReduceCallback callback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object Reduce(ArrayReduceCallback callback, object initialValue);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object Reduce(ArrayItemReduceCallback callback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object Reduce(ArrayItemReduceCallback callback, object initialValue);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object ReduceRight(ArrayReduceCallback callback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object ReduceRight(ArrayReduceCallback callback, object initialValue);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object ReduceRight(ArrayItemReduceCallback callback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object ReduceRight(ArrayItemReduceCallback callback, object initialValue);
 
         // This method is static in the CLR -> void Reverse(Array array)
         public extern void Reverse();
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern object Shift();
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Slice(int start);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern Array Slice(int start, int end);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern bool Some(ArrayFilterCallback filterCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern bool Some(ArrayItemFilterCallback itemFilterCallback);
 
         // All of the Sort methods in CLR are static.
@@ -134,23 +134,23 @@ namespace System
         // This should be -> static void Sort(Array array, IComparer? comparer)
         public extern void Sort(CompareCallback compareCallback);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern void Splice(int start, int deleteCount);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern void Splice(int start, int deleteCount, params object[] itemsToInsert);
 
         [DSharpScriptMemberName("array")]
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern static Array ToArray(object o);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern void Unshift(params object[] items);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern static explicit operator ArrayList(Array array);
 
-        [Obsolete("Not compliant with .NET standard", /* error */ true)]
+        [Obsolete("Not compliant with .NET standard", error: false)]
         public extern static explicit operator List<object>(Array array);
     }
 }
