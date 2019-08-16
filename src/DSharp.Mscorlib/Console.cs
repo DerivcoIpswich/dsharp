66d49026b5a7e63a9bf10e335084a47c02dabebd
FredP <Fred.Perkins@derivco.co.uk>
2019-08-16 12:22:19 +0100

    Resolving an issue with native objects generating the incorrect indexer

--- src/DSharp.Mscorlib/Console.cs
+++ src/DSharp.Mscorlib/Console.cs
@@ -7,7 +7,49 @@ namespace System
     [ScriptName("console")]
     public static class Console
     {
-        [ScriptName("debug")]
+        private const string CONSOLE_LOGGING_METHOD = "debug";
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine();
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(bool value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(char value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(char[] buffer);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
         public extern static void WriteLine(string message);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(double value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(float value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(decimal value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(ushort value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(short value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(uint value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(int value);
+
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(ulong value);
+
+        [ScriptName(CONSOLE_LOGGING_METHOD)]
+        public extern static void WriteLine(long value);
     }
 }
