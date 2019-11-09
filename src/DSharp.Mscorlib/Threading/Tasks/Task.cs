4258753d0c35ae1c2e5ae29d3a9f2e5a75c5b757
Fredrik Nilsson <fredrik.nilsson@combination.se>
2019-11-09 16:26:27 +0100

    Added Task to mscorlib

---
+++ src/DSharp.Mscorlib/Threading/Tasks/Task.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Threading.Tasks
+{
+    [ScriptImport]
+    [ScriptName("Promise")]
+    public sealed class Task
+    {
+        public TaskAwaiter GetAwaiter()
+        {
+            return null;
+        }
+    }
+
+    [ScriptImport]
+    [ScriptName("Promise")]
+    public sealed class Task<TResult>
+    {
+        public TaskAwaiter<TResult> GetAwaiter()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/DSharp.Mscorlib/Threading/Tasks/TaskAwaiter.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Threading.Tasks
+{
+    public sealed class TaskAwaiter : INotifyCompletion
+    {
+        public bool IsCompleted { get; }
+
+        public void OnCompleted(Action continuation)
+        {
+        }
+
+        public void GetResult()
+        {
+        }
+    }
+
+    public sealed class TaskAwaiter<TResult> : INotifyCompletion
+    {
+        public bool IsCompleted { get; }
+
+        public void OnCompleted(Action continuation)
+        {
+        }
+
+        public TResult GetResult()
+        {
+            return default(TResult);
+        }
+    }
+}
