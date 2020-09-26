cc9f33f7b5a6f6ad01f44647a57b54c517a93f08
Kevin Reed <kevin.reed@derivco.co.uk>
2020-09-26 01:29:45 +0100

    added attributes to attributes

--- src/DSharp.Mscorlib/Runtime/CompilerServices/IteratorStateMachineAttribute.cs
+++ src/DSharp.Mscorlib/Runtime/CompilerServices/IteratorStateMachineAttribute.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [ScriptIgnore]
     public sealed class IteratorStateMachineAttribute : StateMachineAttribute
     {
         public IteratorStateMachineAttribute(Type stateMachineType)
--- src/DSharp.Mscorlib/Runtime/CompilerServices/StateMachineAttribute.cs
+++ src/DSharp.Mscorlib/Runtime/CompilerServices/StateMachineAttribute.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [ScriptIgnore]
     public class StateMachineAttribute : Attribute
     {
         public Type StateMachineType { get; private set; }
