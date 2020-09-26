89d9953624fc58bce2d86d35d88e805480ace80d
Kevin Reed <kevin.reed@derivco.co.uk>
2020-09-26 01:20:08 +0100

    added classes required for csc code generation

---
+++ src/DSharp.Mscorlib/Environment.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class Environment
+    {
+        public static int CurrentManagedThreadId
+        {
+            get { throw new NotSupportedException(); }
+        }
+    }
+}
